06afb39cec5285d5d9a56847781125eb49e2ec59
Gordon Maloney <gordonmaloney@gmail.com>
2021-08-08 16:34:00 +0100

    Moved form to Redux, fixed modal closing

--- src/components/NewLingComponent.js
+++ src/components/NewLingComponent.js
@@ -1,30 +1,105 @@
-import React from "react";
-import { Form, FormGroup, Label, Input } from "reactstrap";
+import React, { Component } from "react";
+import { Label, Input, Row, Button, ModalBody } from "reactstrap";
+import { Control, LocalForm } from "react-redux-form";
 
-function NewLingComponent() {
-  return (
-    <Form>
-      <FormGroup>
-        <Label for="ling-text">Ling Text</Label>
-        <Input type="textarea" placeholder="Type your ling here..." className="mb-3"></Input>
-        <Label for="ling-lang-select">Select your language</Label>
-        <Input type="select" className="mb-3">
-          <option>...</option>
-          <option>Scottish Gaelic</option>
-          <option>Spanish</option>
-          <option>German</option>
-          <option>Portuguese</option>
-        </Input>
-        <Label for="correction-pref">Correction Preference</Label>
-        <Input type="select" name="correction-pref" id="correction-pref">
-          <option>...</option>
-          <option>Strict - please correct any errors</option>
-          <option>Relaxed - only correct more significant mistakes</option>
-          <option>Chill - please don't correct me</option>
-        </Input>
-      </FormGroup>
-    </Form>
-  );
+class NewLingComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newLingBody: "",
+      newLingLang: "",
+      newLingCorPref: "",
+      touched: {
+        newLingBody: false,
+        newLingLang: false,
+        newLingCorPref: false,
+      },
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(values) {
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <ModalBody>
+          <div className="container">
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Row className="form-group">
+                <Label htmlFor="newLingLang">Type your ling here</Label>
+
+                <Control.textarea
+                  model=".newLingBody"
+                  id="newLingBody"
+                  name="newLingBody"
+                  placeholder="Type your ling here..."
+                  className="mb-3 form-control"
+                  rows="3"
+                />
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="newLingLang">Select your language</Label>
+                <br />
+                <Control.select
+                  model=".newLingLang"
+                  id="newLingLang"
+                  name="newLingLang"
+                  type="select"
+                  className="mb-3 form-control"
+                >
+                  <option>...</option>
+                  <option>Scottish Gaelic</option>
+                  <option>Spanish</option>
+                  <option>German</option>
+                  <option>Portuguese</option>
+                </Control.select>
+              </Row>
+              <Row className="form-group">
+                <Label htmlFor="newLingCorPref">Correction Preference</Label>
+                <br />
+                <Control.select
+                  type="select"
+                  name="newLingCorPref"
+                  id="newLingCorPref"
+                  model=".newLingCorPref"
+                  className="mb-3 form-control"
+                >
+                  <option>...</option>
+                  <option>Strict - please correct any errors</option>
+                  <option>
+                    Relaxed - only correct more significant mistakes
+                  </option>
+                  <option>Chill - please don\'t correct me</option>
+                </Control.select>
+              </Row>
+
+              <Row className="d-flex flex-row-reverse border-top pt-3">
+                <Button color="secondary" onClick={this.props.close} outline>
+                  Cancel
+                </Button>
+
+                <Button
+                  type="submit"
+                  color="primary"
+                  outline
+                  onClick={this.props.close}
+                  className="mx-2"
+                >
+                  Post Ling
+                </Button>
+              </Row>
+            </LocalForm>
+          </div>
+        </ModalBody>
+      </React.Fragment>
+    );
+  }
 }
 
 export default NewLingComponent;
--- src/components/NewLingModal.js
+++ src/components/NewLingModal.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import NewLingComponent from './NewLingComponent.js'
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import NewLingComponent from "./NewLingComponent.js";
 
 const NewLingModal = (props) => {
   const [modal, setModal] = useState(false);
@@ -8,19 +8,16 @@ const NewLingModal = (props) => {
 
   return (
     <div>
-      <Button color="primary" onClick={toggle} outline className="menu-btn">New Ling</Button>
+      <Button color="primary" onClick={toggle} outline className="menu-btn">
+        New Ling
+      </Button>
       <Modal isOpen={modal} toggle={toggle} className="new-ling-modal">
         <ModalHeader toggle={toggle}>New Ling</ModalHeader>
-        <ModalBody>
-            <NewLingComponent />
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle} outline>Post Ling</Button>{' '}
-          <Button color="secondary" onClick={toggle} outline>Cancel</Button>
-        </ModalFooter>
+
+        <NewLingComponent close={toggle} />
       </Modal>
     </div>
   );
-}
+};
 
 export default NewLingModal;

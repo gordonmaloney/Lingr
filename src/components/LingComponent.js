063b2f8311c0185dc6b7aad9a9605585cb3f7dc5
Gordon Maloney <gordonmaloney@gmail.com>
2021-08-17 21:12:49 +0100

    timeline notification

--- src/components/LingComponent.js
+++ src/components/LingComponent.js
@@ -2,64 +2,61 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Card, CardHeader, CardBody, CardFooter } from "reactstrap";
 import { Link } from "react-router-dom";
-import { actions, Control } from 'react-redux-form';
-
-
-
+import { actions, Control } from "react-redux-form";
 
 class Ling extends Component {
   LingsList() {
     return this.props.lings.lings.reverse().map((ling) => {
       if (this.props.lang === "Show All") {
         return (
-          <div key={ling.id}>
-            <Card className="ling mb-3">
-              <CardHeader>
-                <span className="ling-date">{ling.lingLang}</span>
-                <h3>
-                  {ling.userIcon} - {ling.userName}
-                </h3>
-              </CardHeader>
-              <CardBody>{ling.lingBody}</CardBody>
-              <div className="cor-pref-timeline">
-                <center>
+          <div key={ling.id} className="timeline-hover">
+            <Link to={`/reply/${ling.id}`} className="timeline-no-link">
+              <Card className="ling mb-3">
+                <CardHeader>
+                  <span className="ling-date">{ling.lingLang}</span>
+                  <h3>
+                    {ling.userIcon} - {ling.userName}
+                  </h3>
+                </CardHeader>
+                <CardBody>{ling.lingBody}</CardBody>
+                <div className="cor-pref-timeline">
+                  <center>
                     Correction preference: <b>{ling.lingCorPref}</b>
-                </center>
-              </div>
-              <CardFooter>
-                <Link to={`/reply/${ling.id}`}>
-                  {
-                    this.props.lings.replies.filter(
-                      (reply) => reply.parentId === ling.id).filter(
-                        (reply) => reply.replyType === "reply"
-                      ).length
-                  }{" "}
-
-                  Replies
-                </Link>{" "}
-                |
-                <Link to={`/reply/${ling.id}`}>
-                  {" "}
-                  {
-                    this.props.lings.replies.filter(
-                      (reply) => reply.parentId === ling.id).filter(
-                        (reply) => reply.replyType === "correction"
-                      ).length
-                  }{" "}
-                  Corrections
-                </Link>
-                <span className="ling-date">{ling.lingDate}</span>
-              </CardFooter>
-              <CardBody className="ling-reply">
-                <Link to={`/reply/${ling.id}`}>
-                  <input
-                    className="ling-reply-text"
-                    type="text"
-                    placeholder="Type your reply or correction here..."
-                  />
-                </Link>
-              </CardBody>
-            </Card>
+                  </center>
+                </div>
+                <CardFooter>
+                  <Link to={`/reply/${ling.id}`}>
+                    {
+                      this.props.lings.replies
+                        .filter((reply) => reply.parentId === ling.id)
+                        .filter((reply) => reply.replyType === "reply").length
+                    }{" "}
+                    Replies
+                  </Link>{" "}
+                  |
+                  <Link to={`/reply/${ling.id}`}>
+                    {" "}
+                    {
+                      this.props.lings.replies
+                        .filter((reply) => reply.parentId === ling.id)
+                        .filter((reply) => reply.replyType === "correction")
+                        .length
+                    }{" "}
+                    Corrections
+                  </Link>
+                  <span className="ling-date">{ling.lingDate}</span>
+                </CardFooter>
+                <CardBody className="ling-reply">
+                  <Link to={`/reply/${ling.id}`}>
+                    <input
+                      className="ling-reply-text"
+                      type="text"
+                      placeholder="Type your reply or correction here..."
+                    />
+                  </Link>
+                </CardBody>
+              </Card>
+            </Link>
           </div>
         );
       } else if (this.props.lang === ling.lingLang) {
@@ -75,7 +72,7 @@ class Ling extends Component {
               <CardBody>{ling.lingBody}</CardBody>
               <div className="cor-pref-timeline">
                 <center>
-                    Correction preference: <b>{ling.lingCorPref}</b>
+                  Correction preference: <b>{ling.lingCorPref}</b>
                 </center>
               </div>
               <CardFooter>
@@ -125,6 +122,5 @@ function mapStateToProps(state) {
     lings: state.lings,
   };
 }
-
 
 export default connect(mapStateToProps)(Ling);
